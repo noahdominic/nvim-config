5a2f139833ee9114d50042a6b9bbb352fda8da03
Noah Dominic <noahdmnc@gmail.com>
2024-04-01 09:11:58 +0800

    CSTYLE: Add commentss in plygins.lua

--- lua/noahdominic/init.lua
+++ lua/noahdominic/init.lua
@@ -4,3 +4,5 @@ require('noahdominic.remap')
 require'lspconfig'.rust_analyzer.setup{}
 require'lspconfig'.tsserver.setup{}
 require'lspconfig'.svelte.setup{}
+
+require'ibl'.setup()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,13 +2,17 @@ vim.cmd 'packadd packer.nvim'
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-
+	
+	-- Fuzzy finder
 	use {'nvim-telescope/telescope.nvim', tag = '0.1.5', requires = { {'nvim-lua/plenary.nvim'} } }
 	
+	-- Syntax analysis
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
+	-- Language server
 	use('neovim/nvim-lspconfig')
 
+	-- Autocomplete
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/cmp-path'
