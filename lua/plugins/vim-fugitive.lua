df848a022315d46388f4f105e95b285709548086
noahdominic <noahdmnc@gmail.com>
2024-07-05 08:41:56 +0800

    conf: Add new keymaps for vim-fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,3 +1,8 @@
 local kmp = vim.keymap
 
-kmp.set('n', '<leader>g', ':G ', {})
+kmp.set('n', '<leader>gg', ':G ', {})
+kmp.set('n', '<leader>gs', ':G status<CR>', {})
+kmp.set('n', '<leader>gc', ':G commit<CR>', {})
+kmp.set('n', '<leader>ga', ':G add ', {})
+kmp.set('n', '<leader>gpush', ':G push<CR>', {})
+kmp.set('n', '<leader>gpull', ':G pull<CR>', {})
