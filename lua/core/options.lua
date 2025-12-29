ff268b8fd0ce190a012c49103d23cf54e584eb8e
noahdominic <noahdmnc@gmail.com>
2025-12-30 04:51:14 +0800

    Remove config that automatically converts \t to spaces

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,8 +7,9 @@ vim.opt.relativenumber = true
 -- Tabs
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true -- Replace tab characters w/ spaces
+-- vim.opt.expandtab = true -- Replace tab characters w/ spaces
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- Search settings
 vim.opt.ignorecase = true
