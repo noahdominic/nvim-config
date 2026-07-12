ae8e1e8d476303386a9d929c3a53b9674c6937c1
noahdominic <noahdmnc@gmail.com>
2026-07-12 19:00:04 +0800

    (temp) Commenting out stuff i dont understand

--- init.lua
+++ init.lua
@@ -13,3 +13,20 @@ require('plugins.nvim-lspconfig')
 require('plugins.vim-fugitive')
 require('plugins.catppuccin')
 require('plugins.mason')
+
+-- vim.opt.textwidth = 80
+-- vim.opt.formatoptions:append("t")
+-- 
+-- vim.opt.joinspaces = false
+-- 
+-- vim.api.nvim_create_augroup("prose", { clear = true })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = "prose",
+--   pattern = { "text", "markdown", "gitcommit" },
+--   callback = function()
+--     vim.opt_local.textwidth = 80
+--     vim.opt_local.formatoptions:append("t")
+--     vim.opt_local.joinspaces = false
+--   end,
+-- })
+
